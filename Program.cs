e8b0fb8a946591333fa2e1aac683a194f230a27b
HenryKasai <henrykasai1775@gmail.com>
2020-03-13 22:39:20 -0300

    Exercício Resolvido

--- Program.cs
+++ Program.cs
@@ -8,7 +8,20 @@ namespace Fleet
         {
             Console.WriteLine("Tradutor de Leet Speak, escreva algo.");
             string texto = Console.ReadLine();
-            Console.WriteLine(texto .Replace("A", "4").Replace("a", "4") .Replace("E", "3") .Replace("e","3").Replace("I","1").Replace("i","1").Replace("L","1").Replace("l","1").Replace("O","0").Replace("o","0").Replace("T","7").Replace("t","7").Replace("S","5").Replace("s","5"));
+            Console.WriteLine(texto .Replace("A", "4")
+            .Replace("a", "4") 
+            .Replace("E", "3") 
+            .Replace("e","3")
+            .Replace("I","1")
+            .Replace("i","1")
+            .Replace("L","1")
+            .Replace("l","1")
+            .Replace("O","0")
+            .Replace("o","0")
+            .Replace("T","7")
+            .Replace("t","7")
+            .Replace("S","5")
+            .Replace("s","5"));
         }
     }
 }
